e8624db98d50296a1c42fb2a2b252c543f476874
nociza <azicon@berkeley.edu>
2023-06-29 17:29:25 +0800

    first relic

---
+++ frontend/posts.js
@@ -0,0 +1,108 @@
+// Dummy posts created by steven for prototyping June 2022
+// Left in the "Asset" folder of the frontend for a whole year before rediscovered
+exports.posts = [
+  {
+    userName: "Steven",
+    content:
+      "两年前我拿着访客码第一次来腾讯谈合作，两年后我带着鹅厂工牌来深圳总部报道",
+    userImage:
+      "https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    postImages: [
+      "https://cdn.mos.cms.futurecdn.net/Ev3irSe2WxYCdyGSx6rDES-1200-80.jpg",
+      "https://ggsc.s3.amazonaws.com/images/uploads/The_Science-Backed_Benefits_of_Being_a_Dog_Owner.jpg",
+    ],
+    caption: "努力工作也是享受",
+    likes: 2328,
+    comments: [
+      "skjdflkjdslkjfkljsdljfjlkds",
+      "hkdghsfkjhkjdshfkjhsdjhfksdjhf",
+      "dkjhsfkjhkjdhfhskjdhfkjhsdkj",
+    ],
+    collection: 100,
+  },
+  {
+    userName: "Peter",
+    content: "今天也要好好学习！",
+    userImage:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80",
+    postImages: [
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/heart-healthy-foods-1644800263.jpg?crop=1.00xw:0.801xh;0,0&resize=640:*",
+      "https://imagesvc.meredithcorp.io/v3/jumpstartpure/image?url=https://cf-images.us-east-1.prod.boltdns.net/v1/static/5118192885001/b2137dc8-a461-411d-a151-a31810fbf5e0/eafb39ac-f2c5-4f00-b793-99b7114c1985/1280x720/match/image.jpg&w=1280&h=720&q=90&c=cc",
+      "https://www.foodnavigator.com/var/wrbm_gb_food_pharma/storage/images/_aliases/wrbm_large/publications/food-beverage-nutrition/foodnavigator.com/news/market-trends/ultra-processing-foods-for-health-and-sustainability-whether-you-like-it-or-not-processed-food-is-here-to-stay/13222691-1-eng-GB/Ultra-processing-foods-for-health-and-sustainability-Whether-you-like-it-or-not-processed-food-is-here-to-stay.jpg",
+    ],
+    caption: "今天也要好好学习！",
+    likes: 10243,
+    comments: [
+      "jksdhfjkhsjdhfkjhsdjfkhdsjkhfjksd",
+      "sdjfjsdhkjhskdjfhjksdjfkjds",
+    ],
+    collection: 300,
+  },
+  {
+    userName: "Cindy",
+    content: "jkdhsjfhkjdshkfhdsjfkjdshkfjhdshfkjdskfhjkdshjkfhdsjkhfds",
+    userImage:
+      "https://expertphotography.b-cdn.net/wp-content/uploads/2018/10/cool-profile-pictures-retouching-1.jpg",
+    postImages: [
+      "https://bookriot.com/wp-content/uploads/2022/02/stack-of-books-in-a-bookstore-1280x720.png",
+      "https://ychef.files.bbci.co.uk/976x549/p03gg1lc.jpg",
+      "https://cdn.pixabay.com/photo/2015/11/19/21/10/glasses-1052010__340.jpg",
+      "https://images.unsplash.com/photo-1561657819-51c0511e35ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Ym9va3N8ZW58MHx8MHx8&w=1000&q=80",
+    ],
+    caption: "Learn how to read books",
+    likes: 20039,
+    comments: ["ksjdlfkjklsdjlfkjkdsjlkfjsdlkjfkds"],
+    collection: 0,
+  },
+  {
+    userName: "Terry",
+    content: "jkdhsjfhkjdshkfhdsjfkjdshkfjhdshfkjdskfhjkdshjkfhdsjkhfds",
+    userImage:
+      "https://d2qp0siotla746.cloudfront.net/img/use-cases/profile-picture/template_0.jpg",
+    postImages: [
+      "https://cdn.britannica.com/57/183257-050-0BA11B4B/Roger-Federer-2012.jpg",
+      "https://bloximages.newyork1.vip.townnews.com/thesheridanpress.com/content/tncms/assets/v3/editorial/5/b0/5b08a1d8-04e9-11ec-9b52-6f5bc1cb8892/61250514488e5.hires.jpg",
+    ],
+    caption: "Learn how to play tennis",
+    likes: 2003,
+    comments: [
+      "slkjdjflksdjkfljlksdjkflsdkfdsfs",
+      "dksjhfjkhjdhskjhfsdkfjhsdkjhfsd",
+      "sdlkflkjsdlkjfkljsdlkjflkdjlsfsd",
+    ],
+    collection: 1340,
+  },
+  {
+    userName: "Alex",
+    content: "jkdhsjfhkjdshkfhdsjfkjdshkfjhdshfkjdskfhjkdshjkfhdsjkhfds",
+    userImage:
+      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    postImages: [
+      "https://www.ft.com/__origami/service/image/v2/images/raw/https%3A%2F%2Fd1e00ek4ebabms.cloudfront.net%2Fproduction%2Ff6065475-435e-4bca-bdb4-9b4f95a93ea3.jpg?fit=scale-down&source=next&width=700",
+      "https://bloximages.newyork1.vip.townnews.com/thesheridanpress.com/content/tncms/assets/v3/editorial/5/b0/5b08a1d8-04e9-11ec-9b52-6f5bc1cb8892/61250514488e5.hires.jpg",
+    ],
+    caption: "Learn how to code",
+    likes: 2233,
+    comments: [
+      "sjdhjfkhsjkdhjkfkjsdhjkfsjkdfs",
+      "dkisjlfjlksdjlkfjsdljklfjlskdkljlkjlkjsdf",
+    ],
+    collection: 300,
+  },
+  {
+    userName: "Jerry",
+    userImage: "https://miro.medium.com/max/785/0*Ggt-XwliwAO6QURi.jpg",
+    content: "jkdhsjfhkjdshkfhdsjfkjdshkfjhdshfkjdskfhjkdshjkfhdsjkhfds",
+    postImages: [
+      "https://res.cloudinary.com/highereducation/image/upload/v1533591754/TheBestColleges.org/study-notebooks.jpg",
+      "https://bloximages.newyork1.vip.townnews.com/thesheridanpress.com/content/tncms/assets/v3/editorial/5/b0/5b08a1d8-04e9-11ec-9b52-6f5bc1cb8892/61250514488e5.hires.jpg",
+    ],
+    caption: "跟我一起学习吧！",
+    likes: 2233,
+    comments: [
+      "sdjhfkjjdshkjfjsdkfjhsdf",
+      "dskjfkjhdsjhjkfsdhfhjjsdkhfsjsdhkfsd",
+    ],
+    collection: 30,
+  },
+];
